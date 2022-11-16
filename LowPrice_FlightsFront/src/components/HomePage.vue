027967e7fca1530a03d867dea82f7855a2302e64
Grazi998 <grazianokaluza@gmail.com>
2022-11-16 18:42:06 +0100

    App initialized with backend

---
+++ LowPrice_FlightsFront/src/components/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+    <p class="greeting">{{ greeting }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      greeting: 'Hello World!'
+    }
+  }
+}
+</script>
+
+
+
+<style>
+    .greeting {
+        color: red;
+        font-weight: bold;
+    }
+</style>
